6712ba3a61175107aa30b26880c4906e03a58f83
fceciliaaa <ceciferreira52@gmail.com>
2024-06-17 17:33:06 -0400

    react con jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Dojo!</h1>
+      <h3>Cosas que necesito hacer:</h3>
+        <ul>
+          <li>Aprender React</li>
+          <li>Escalar la montaña Everest</li>
+          <li>Correr una maraton</li>
+          <li>Alimentar a los perros</li>
+        </ul>
     </div>
   );
 }
